3f5c0a3bcf58734497d2938122061a64804f2ebb
99002779 <72495880+99002779@users.noreply.github.com>
2020-11-21 10:15:32 +0530

    Update PetNotFoundException.java
    Pet Not Found Exception Handler for the exception package

--- Pet_Care/src/main/java/com/pet/exception/PetNotFoundException.java
+++ Pet_Care/src/main/java/com/pet/exception/PetNotFoundException.java
@@ -2,7 +2,37 @@ package com.pet.exception;
 
  
 
+@SuppressWarnings("serial")
 public class PetNotFoundException extends RuntimeException {
 
+ 
+    public PetNotFoundException() {
+        // TODO Auto-generated constructor stub
+    }
+
+
+    public PetNotFoundException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+ 
+
+    public PetNotFoundException(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+
+    public PetNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+
+    public PetNotFoundException(String message, Throwable cause, boolean enableSuppression,
+            boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        // TODO Auto-generated constructor stub
+    }
 
 }
