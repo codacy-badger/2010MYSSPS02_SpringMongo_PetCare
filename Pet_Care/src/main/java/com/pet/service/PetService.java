6f6fbe27d876a074903912e3dfb8be2145e06eb2
99002776 <72496144+99002776@users.noreply.github.com>
2020-11-21 09:53:39 +0530

    Update PetService.java
    Creating Pet Service Interface to define methods

--- Pet_Care/src/main/java/com/pet/service/PetService.java
+++ Pet_Care/src/main/java/com/pet/service/PetService.java
@@ -1,5 +1,20 @@
 package com.pet.service;
+import org.springframework.stereotype.Service;
+import java.util.List;
 
+import com.pet.exception.PetNotFoundException;
+import com.pet.model.Pet;
+
+@Service
 public interface PetService {
+	Pet addPet(Pet pet);
+    boolean deletePet(Integer petId) throws PetNotFoundException;
+    Pet getPetById(Integer petId) throws PetNotFoundException;
+    Pet updatePet( Pet pet);
+   
+    List<Pet> getAllPets();
+    List<Pet> getPetbyBreed(String author) throws PetNotFoundException;
+    List<Pet> getPetbyCategory(String category)throws PetNotFoundException;
+    
 	
 }
