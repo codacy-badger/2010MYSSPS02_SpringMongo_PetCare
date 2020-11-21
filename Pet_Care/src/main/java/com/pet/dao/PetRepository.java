54b9fecae13ce1365411758675cf381a7c237f18
99002773 <72495902+99002773@users.noreply.github.com>
2020-11-21 09:59:19 +0530

    Update PetRepository.java
    The Data Access Object (DAO) interface is committed successfully..

--- Pet_Care/src/main/java/com/pet/dao/PetRepository.java
+++ Pet_Care/src/main/java/com/pet/dao/PetRepository.java
@@ -1,5 +1,20 @@
 package com.pet.dao;
 
-public interface PetRepository {
+import java.util.List;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import com.pet.model.Pet;
 
+@Repository
+public interface PetRepository extends MongoRepository<Pet,Integer>{
+	//derived query
+	Pet findByPetId(Integer petId);
+	List<Pet> findByPetName(String petName);
+	List<Pet> findByPetCategory(String petCategory);
+	List<Pet> findByPetBreed(String petBreed);
+	List<Pet> findByPetFood(String petFood);
+	
+	
+	List<Pet> findPetByPetBreedAndPetCost(String petBreed,Integer petCost);
+	List<Pet> findPetByPetCategoryAndPetBreed(String petCategory, String petBreed);
 }
